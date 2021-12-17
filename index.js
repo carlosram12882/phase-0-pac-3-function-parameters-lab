ef73291b398fdfef604569310943fa56d05ac8e5
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-17 12:03:26 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function introduction(string){
     return  `Hi, my name is ${string}.`;
 }
 
-function introductionWithLanguage(){ 
-    console.log(arguments[0]);
+function introductionWithLanguage(string, langague){ 
+    return `Hi, my name is ${string} and I am learning to program in ${langague}.`;
 }
-introductionWithLanguage("dom")
+function introductionWithLanguageOptional(string = "Gracie", langague = "JavaScript"){
+    return `Hi, my name is ${string} and I am learning to program in ${langague}.`
+}
